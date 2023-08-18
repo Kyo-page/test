47a5fd67713546790f445fba1fe49b2850277efe
Kyo-page <116732822+Kyo-page@users.noreply.github.com>
2023-08-18 14:57:23 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
-
+document.addEventListener('DOMContentLoaded', function() {
+    var links = document.querySelectorAll('a'); // すべての <a> タグを選択
+    links.forEach(function(link) {
+        link.addEventListener('click', function(event) {
+            event.preventDefault(); // リンクのデフォルトの遷移をキャンセル
+            var delay = 3000; // 遅延するミリ秒数（例: 3秒）
+            setTimeout(function() {
+                window.location.href = link.getAttribute('href'); // 遷移を実行
+            }, delay);
+        });
+    });
+});
 
 
 
